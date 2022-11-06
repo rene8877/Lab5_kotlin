57b54a788f0d294dce26fe909f40914a127f4381
rene8877 <pig2709008@gmail.com>
2022-11-06 15:16:58 +0800

    first commit

---
+++ app/src/main/java/com/example/lab5_k/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.lab5_k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("MainActivity","onCreate")
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        findViewById<ViewPager>(R.id.viewPager).adapter = adapter
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("MainActivity","onRestart")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.e("MainActivity","onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.e("MainActivity","onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.e("MainActivity","onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.e("MainActivity","onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("MainActivity","onDestroy")
+    }
+}
+class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int) = when(position) {
+        0 -> FirstFragment()
+        1 -> SecondFragment()
+        else -> ThirdFragment()
+    }
+    override fun getCount() = 3
+}
